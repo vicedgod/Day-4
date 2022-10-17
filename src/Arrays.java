7774a7c411c9e44e6be6c3911b2ccb0949b731d3
Aman Kumar Behera <110566903+vicedgod@users.noreply.github.com>
2022-10-17 12:19:44 +0530

    Update Arrays.java

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Arrays {
+public class Arrays {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        arr[0]=23;
+        arr[1]=45;
+        arr[2]=69;
+        arr[3]=49;
+        arr[4]=55;
+        System.out.println(arr[3]);
+
+    }
 }
